82ee892466f7f5285735149e8156edff67c5c28c
Skyteks <Schattenpalkia@googlemail.com>
2024-11-13 16:36:51 +0100

    done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import StudentCard from "../components/StudentCard";
-
 import studentsData from "../assets/students.json";
+import { NavLink } from "react-router-dom";
 
 function HomePage() {
   // eslint-disable-next-line no-unused-vars
@@ -21,7 +21,9 @@ function HomePage() {
       {students &&
         students.map((student) => {
           return (
+            <NavLink to="/students/:studentId" studentId={student._id}>
               <StudentCard key={student._id} {...student} />
+            </NavLink>
           );
         })}
     </div>
--- src/pages/StudentDetailsPage.jsx
+++ src/pages/StudentDetailsPage.jsx
@@ -1,5 +1,6 @@
 import placeholderImage from "./../assets/placeholder.png";
 import studentsData from "./../assets/students.json";
+import { NavLink } from "react-router-dom";
 
 function StudentDetailsPage() {
 
@@ -67,9 +68,9 @@ function StudentDetailsPage() {
 
 
             {/* Back button */}
-            <button className="text-white px-4 py-2 rounded bg-green-500 hover:bg-green-600 transition duration-300 ease-in-out">
+            <NavLink to="/" className="text-white px-4 py-2 rounded bg-green-500 hover:bg-green-600 transition duration-300 ease-in-out">
               Back
-            </button>
+            </NavLink>
 
           </>
         )}
